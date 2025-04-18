acaeb427d9cc527e7c4ebed3d02687b07bd17c24
Kim Hani <rlahani99@gmail.com>
2025-04-18 12:07:50 +0900

    init

---
+++ src/main/kotlin/pinggu/portforu_crawler/PortforuCrawlerApplication.kt
@@ -0,0 +1,11 @@
+package pinggu.portforu_crawler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortforuCrawlerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortforuCrawlerApplication>(*args)
+}
---
+++ src/test/kotlin/pinggu/portforu_crawler/PortforuCrawlerApplicationTests.kt
@@ -0,0 +1,13 @@
+package pinggu.portforu_crawler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortforuCrawlerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
